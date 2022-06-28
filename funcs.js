61452c407c60aa3280db83a4839aa6600abe05db
labor-day <shaffer.scott@yahoo.com>
2022-06-27 18:51:55 -0600

    added unit tests

--- funcs.js
+++ funcs.js
@@ -1,13 +1,26 @@
 function isFive(num) {
   // Your code here
+  if (num === 5) {
+    return true;
+  }
+  return false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== "number") {
+    throw new Error;
+  }
+    return (number % 2 !== 0);
+
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  let range = [];
+  for (let i = min; i <= max; i += step) {
+    range.push(i);
+  }
+  return range;
 }
 
 
--- test/class-spec.js
+++ test/class-spec.js
@@ -5,30 +5,56 @@ const { Word } = require("../class");
 
 describe("Word", function () {
   describe("Word constructor function", function () {
+
+    let word;
+    this.beforeEach(() => {
+      word = new Word("whatever");
+    })
+
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word).to.have.property("word");
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.equal("whatever");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+
+      let word1 = new Word("aeiou");
+      let word2 = new Word("zxcvb");
+      let word3 = new Word("whatever");
+
+      expect(word1.removeVowels()).to.equal("");
+      expect(word2.removeVowels()).to.equal("zxcvb");
+      expect(word3.removeVowels()).to.equal("whtvr");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+
+      let word1 = new Word("aeiou");
+      let word2 = new Word("zxcvb");
+      let word3 = new Word("whatever");
+
+      expect(word1.removeConsonants()).to.equal("aeiou");
+      expect(word2.removeConsonants()).to.equal("");
+      expect(word3.removeConsonants()).to.equal("aee");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+
+      let word1 = new Word("bed");
+      let word2 = new Word("end");
+
+      expect(word1.pigLatin()).to.equal("edbay");
+      expect(word2.pigLatin()).to.equal("endyay");
+
     });
   });
 });
